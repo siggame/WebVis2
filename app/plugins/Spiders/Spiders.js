ee41fcf858e28aa9f119df5a342d00c9ff136719
Wyatt Kennedy <wykennedy@gmail.com>
2016-03-11 18:50:51 -0600

    initial spiders push

---
+++ app/plugins/Spiders/Spiders.js
@@ -0,0 +1,13 @@
+//# sourceURL=Spiders.js
+
+(function() {
+    var Spiders = function() {
+        this.__proto__ = new WebVis.plugin.Base;
+
+        this.loadGame = function(date) {
+
+        };
+    };
+
+    WebVis.plugin.addPlugin("Spiders", new Spiders);
+})();
