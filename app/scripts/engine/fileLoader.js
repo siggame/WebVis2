210b5014f50232e22980de10f06441d6feb0fb0c
Wyatt Kennedy <wykennedy@gmail.com>
2016-05-03 15:29:13 -0500

    commenting fileLoader.js and refactoring

--- app/scripts/engine/fileLoader.js
+++ app/scripts/engine/fileLoader.js
@@ -1,7 +1,14 @@
-WebVis.ready(function() {
+// The contents of this file are responsible for loading files from either
+// a file that has been dropped on the client, or a file that is downloaded
+// from a url
+// TODO: Should probably remove support for .gamelog and .glog files
 
+WebVis.ready(function() {
     var acceptedFileExtensions = ["gamelog", "glog", "json"];
 
+    // this obtains the file extension of the provide file.
+    // If it is an accepted file extension, then it is returned
+    // if it is not accepted, and empty string is returned
     var verifyFileType = function(filename) {
         var ext = "";
         for(var i = 0; i < acceptedFileExtensions.length; i++) {
@@ -13,6 +20,9 @@ WebVis.ready(function() {
         return ext;
     };
 
+    // this function takes the files dropped on the webvis and determines
+    // if they are valid file types. If it is, it returns the file and its
+    // file extension. If not, an exception is thrown.
     var checkDroppedFiles = function(files) {
         if(files.length != 1) {
             throw "Multiple files dropped";
@@ -32,6 +42,9 @@ WebVis.ready(function() {
         };
     };
 
+    // DEPRECATED
+    // This function is used to take glog files and decrompress them
+    // to a usable file type
     var readCompressed = function(fileData, callback) {
         var prepareFile = function(event) {
             var file = {
@@ -53,6 +66,8 @@ WebVis.ready(function() {
         reader.onload = decompress;
     };
 
+    // receives the filedata as a buffer and turns it into a proper string.
+    // when the file reader is done, it passes this data to the provide callback.
     var readDecompressed = function(fileData, callback) {
         reader = new FileReader();
         reader.onload = function(event) {
@@ -66,6 +81,9 @@ WebVis.ready(function() {
         reader.readAsText(fileData.file);
     };
 
+    // DEPRECATED
+    // This is used to determine if the file is of a compressed or uncompressed
+    // and calls the appropriate read function
     var preProcessFile = function(file, callback) {
         if(file.extension === "glog") {
             readCompressed(file, callback);
@@ -74,84 +92,93 @@ WebVis.ready(function() {
         }
     };
 
-    WebVis.fileLoader = {
-        loadFile: function(files, callback) {
-            try {
-                var fileData = checkDroppedFiles(files);
-                preProcessFile(fileData, callback);
-            } catch(error) {
-                console.log(error);
-            }
-        },
-
-        loadFromUrl: function(u, callback) {
-            var urlsplit = u.split(".");
-
-            console.log("GETTING FILE FROM " + u);
-
-            var checkExtension = function(url) {
-                var a = url.split('.');
-                if(a.length === 1 || a[1] === "") {
-                    return "";
-                }
-                return verifyFileType("." + a.pop());
-            };
-
-            var error = function(jqxhr, textStatus, errorThrown) {
-                WebVis.alert("Danger", "File could not be loaded from " + u);
-            }
-
-            var success = function(data) {
-                file = {
-                    extension: ext,
-                    file: new Blob([data])
-                };
-                preProcessFile(file, callback);
-            };
-
-            var fetchText = function() {
-                $.ajax({
-                    type: "GET",
-                    dataType: "text",
-                    url: u,
-                    data: null,
-                    success: success,
-                    error: error
-                });
-            };
-
-            var fetchBinary = function() {
-                $.ajax({
-                    type: "GET",
-                    dataType: "text",
-                    url: u,
-                    data: null,
-                    success: success,
-                    error: error
-                });
-            };
-
-            try {
-                var ext = checkExtension(u);
-                switch(ext) {
-                    case "glog":
-                        fetchBinary();
-                        break;
-                    case "gamelog":
-                        fetchText();
-                        break;
-                    case "json":
-                        fetchText();
-                        break;
-                    default:
-                        fetchBinary();
-                        break;
-                }
-            } catch(error) {
-                console.log(error);
-            }
-
+    // This function is made available to the WebVis and takes in the file that
+    // ws dropped onto the webvis and passes the data out as a string via the
+    // provided callback
+    var loadFile = function(files, callback) {
+        try {
+            var fileData = checkDroppedFiles(files);
+            preProcessFile(fileData, callback);
+        } catch(error) {
+            console.log(error);
         }
+    };
+
+    // This function is made available to the WebVis and takes a url, (as a string),
+    // downloads the files using an ajax call, and then processes the file. The
+    // complete string data is passed as the first parameter to the callback.
+    var loadFromUrl = function(u, callback) {
+        var urlsplit = u.split(".");
+
+        console.log("GETTING FILE FROM " + u);
+
+        var checkExtension = function(url) {
+            var a = url.split('.');
+            if(a.length === 1 || a[1] === "") {
+                return "";
+            }
+            return verifyFileType("." + a.pop());
+        };
+
+        var error = function(jqxhr, textStatus, errorThrown) {
+            WebVis.alert("Danger", "File could not be loaded from " + u);
+        }
+
+        var success = function(data) {
+            file = {
+                extension: ext,
+                file: new Blob([data])
+            };
+            preProcessFile(file, callback);
+        };
+
+        var fetchText = function() {
+            $.ajax({
+                type: "GET",
+                dataType: "text",
+                url: u,
+                data: null,
+                success: success,
+                error: error
+            });
+        };
+
+        var fetchBinary = function() {
+            $.ajax({
+                type: "GET",
+                dataType: "text",
+                url: u,
+                data: null,
+                success: success,
+                error: error
+            });
+        };
+
+        try {
+            var ext = checkExtension(u);
+            switch(ext) {
+                case "glog":
+                    fetchBinary();
+                    break;
+                case "gamelog":
+                    fetchText();
+                    break;
+                case "json":
+                    fetchText();
+                    break;
+                default:
+                    fetchBinary();
+                    break;
+            }
+        } catch(error) {
+            console.log(error);
+        }
+
+    };
+
+    WebVis.fileLoader = {
+        loadFile: loadFile,
+        loadFromUrl: loadFromUrl
     };
 
 });
